a323d2a4a77596859f6b5d9ff1be7905351a759d
Sanchit Chavan <sanchitc1010@gmail.com>
2024-07-29 18:44:53 +0530

    make responsive

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const container = document.querySelector('.container');
 const search = document.querySelector('.search-box button');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
+const cityInput = document.querySelector('.search-box input');
+
+
 
 search.addEventListener('click', () => {
     const APIKey = '98740f4ebc0d63bc0f8ba70090e5a091';
@@ -26,20 +29,25 @@ search.addEventListener('click', () => {
                     break;
                 case 'Rain':
                     image.src = '/Images/rain.png';
-                    document.body.style.backgroundImage = "url('/Images/240_F_823017121_nb9kE9UxuLNwvaZfrQKFMg8WexcJ1oJm.jpg')";
+                    document.body.style.backgroundImage = "url('/Images/rain.jpg')";
                     break;
                 case 'Snow':
                     image.src = '/Images/snow.png';
-                    document.body.style.backgroundImage = "url('/Images/snow-background.jpg')";
+                    document.body.style.backgroundImage = "url('/Images/snow.jpg')";
                     break;
                 case 'Clouds':
                     image.src = '/Images/cloud.png';
                     document.body.style.backgroundImage = "url('/Images/240_F_747169629_Z51nHt57EndryEjdLgDTCAqDziyrQrna.jpg')";
                     break;
                 case 'Haze':
-                        image.src = '/Images/snow.png';
-                        document.body.style.backgroundImage = "url('/Images/snow-background.jpg')";
-                        break;    
+                    image.src = '/Images/Haze.png';
+                    document.body.style.backgroundImage = "url('/Images/Haze.jpg')";
+                    break; 
+                case 'Mist':
+                    image.src = '/Images/mist.png';
+                    document.body.style.backgroundImage = "url('/Images/mist.jpg')";
+                    break;      
+
                 default:
                     image.src = '/Images/clear.png';
                     document.body.style.backgroundImage = "url('/Images/abstract-background-with-smooth-waves-in-shades-of-black.png')";
@@ -52,4 +60,13 @@ search.addEventListener('click', () => {
             wind.innerHTML = `${json.wind.speed} Km/h`;
         })
         .catch(error => alert('Error fetching weather data: ' + error));
+
+
 });
+
+
+cityInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        search.click();
+    }
+}); 
